6a39a8d01dcd8d3d26e52f7f478063424035cd0c
radi_sh <masamory7@gmail.com>
2019-12-28 14:36:49 +0900

    [common] DebugLog関係のコード更新

--- common.h
+++ common.h
@@ -10,44 +10,48 @@
 
 using QWORD = unsigned long long;
 
-extern FILE *g_fpLog;
-
-static inline void SetDebugLog(std::wstring sLogPath)
+namespace DebugLog
 {
-	if (g_fpLog) {
-		// 別のインスタンスにより開かれている
+	inline FILE* fpLog = NULL;
+	inline void SetDebugLog(std::wstring sLogPath)
+	{
+		if (fpLog) {
+			// 別のインスタンスにより開かれている
+			return;
+		}
+
+		fpLog = _wfsopen(sLogPath.c_str(), L"a+", _SH_DENYNO);
+
 		return;
 	}
 
-	g_fpLog = _wfsopen(sLogPath.c_str(), L"a+", _SH_DENYNO);
+	inline void CloseDebugLog(void)
+	{
+		if (fpLog) {
+			fclose(fpLog);
+			fpLog = NULL;
+		}
 
-	return;
-}
-
-static inline void CloseDebugLog(void)
-{
-	if (g_fpLog) {
-		fclose(g_fpLog);
-		g_fpLog = NULL;
+		return;
 	}
 
-	return;
-}
+	inline void OutputDebug(LPCWSTR format, ...)
+	{
+		WCHAR buffer[2048];
+		va_list ap;
+		va_start(ap, format);
+		::vswprintf_s(buffer, sizeof(buffer) / sizeof(buffer[0]), format, ap);
+		va_end(ap);
+		::OutputDebugStringW(buffer);
 
-static inline void OutputDebug(LPCWSTR format, ...)
-{
-	WCHAR buffer[2048];
-	va_list ap;
-	va_start(ap, format);
-	::vswprintf_s(buffer, sizeof(buffer) / sizeof(buffer[0]), format, ap);
-	va_end(ap);
-	::OutputDebugStringW(buffer);
-
-	if (g_fpLog) {
-		fwprintf(g_fpLog, buffer);
-		fflush(g_fpLog);
+		if (fpLog) {
+			fwprintf(fpLog, buffer);
+			fflush(fpLog);
+		}
 	}
 }
+
+using namespace DebugLog;
 
 namespace common
 {
